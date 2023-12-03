08c928ade1a19a6100e297817e3b3727b875bbc4
risktoollib <pcote@ualberta.ca>
2023-12-03 16:44:05 -0700

    2023 12 03

--- extras.sh
+++ extras.sh
@@ -4,5 +4,6 @@
 sudo apt update
 
 # Install required packages
-sudo apt install -y libavfilter-dev librsvg2-dev poppler-utils cargo libmagick++-dev libpoppler-cpp-dev chromium-bsu
+sudo apt install -y libavfilter-dev librsvg2-dev poppler-utils cargo libmagick++-dev libpoppler-cpp-dev libtesseract-dev libleptonica-dev tesseract-ocr-eng
 npx playwright install
+npm install cheerio
