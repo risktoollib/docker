d55c043ff54cfce0bb3cf76fed9c65a94e0521a5
Philippe Cote <pcote@ualberta.ca>
2023-12-11 09:29:09 -0700

    2023 12 11

--- extras.sh
+++ extras.sh
@@ -4,6 +4,17 @@
 sudo apt update
 
 # Install required packages
-sudo apt install -y libavfilter-dev librsvg2-dev poppler-utils cargo libmagick++-dev libpoppler-cpp-dev libtesseract-dev libleptonica-dev tesseract-ocr-eng
+sudo apt install -y libopenblas-base libopenblas-dev libavfilter-dev librsvg2-dev poppler-utils cargo libmagick++-dev libpoppler-cpp-dev libtesseract-dev libleptonica-dev tesseract-ocr-eng
+sudo apt-get autoremove
+
+# OpenBlas
+#sudo apt install -y libopenblas-base libopenblas-dev
+#export LD_LIBRARY_PATH=/usr/lib/openblas-base/
+
+# rQuantlib
+sudo apt install libquantlib0v5 libquantlib0-dev
+Rscript -e "install.packages('rQuantlib')"
+
+# node packages
 npx playwright install
 npm install cheerio
