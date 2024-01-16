f612b35c30fa67258c1650afbcd3615f81b68f8b
Philippe Cote <pcote@ualberta.ca>
2024-01-16 06:44:15 -0700

    reticulate python3.11

--- extras.sh
+++ extras.sh
@@ -25,5 +25,8 @@ npm install cheerio axios
 # quarto pdf
 quarto install tinytex
 
+# reticulare python3.11
+Rscript -e 'library(reticulate);reticulate::install_python(version = '3.11')'
+
 # fOptions
 #Rscript -e 'install.packages("fOptions", repos="http://R-Forge.R-project.org"); install.packages("fExoticOptions", repos="http://R-Forge.R-project.org")'
