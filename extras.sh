f680c9a4779ebf8f7782e4041399a0cfafdb2b38
Philippe Cote <pcote@ualberta.ca>
2024-03-04 07:40:44 -0700

    2024 03 04

--- extras.sh
+++ extras.sh
@@ -4,6 +4,7 @@
 #sed -i 's/\r//' extras.sh
 
 # Update package list
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoclean && sudo apt-get autoremove
 
 # Install required packages
