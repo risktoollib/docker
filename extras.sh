d64ad52007d8bcff6ba649b34d10dfb0ca3843dd
Philippe Cote <pcote@ualberta.ca>
2024-03-11 12:54:30 -0600

    2024 03 11

--- extras.sh
+++ extras.sh
@@ -4,19 +4,14 @@
 #sed -i 's/\r//' extras.sh
 
 # Update package list
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoclean && sudo apt-get autoremove
 
 # Install required packages
 # sudo apt install -y jags libavfilter-dev librsvg2-dev poppler-utils cargo libmagick++-dev libpoppler-cpp-dev libtesseract-dev libleptonica-dev tesseract-ocr-eng
 
-# OpenBlas
-#sudo apt install -y libopenblas-base libopenblas-dev
-#export LD_LIBRARY_PATH=/usr/lib/openblas-base/
-
 # rQuantlib
-sudo apt install libquantlib0v5 libquantlib0-dev
-Rscript -e 'pkgs <- c("available"); installed_pkgs <- rownames(installed.packages()); install.packages(pkgs[!(pkgs %in% installed_pkgs)], dependencies = TRUE)'
+#sudo apt install libquantlib0v5 libquantlib0-dev
+#Rscript -e 'pkgs <- c("available"); installed_pkgs <- rownames(installed.packages()); install.packages(pkgs[!(pkgs %in% installed_pkgs)], dependencies = TRUE)'
 
 # node packages
 npx playwright install
